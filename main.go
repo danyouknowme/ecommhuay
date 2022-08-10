7ac4f535942d396bbd1afa7ae6d8c2b21d456adc
danyouknowme <thanathip.suw@gmail.com>
2022-08-11 03:34:07 +0700

    Connect database and start app

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "github.com/danyouknowme/ecommerce-api/pkg/app"
+import (
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/danyouknowme/ecommerce-api/pkg/app"
+	"github.com/danyouknowme/ecommerce-api/pkg/database"
+	"github.com/danyouknowme/ecommerce-api/pkg/util"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	fiberApp := app.CreateFiberApp()
+
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Connot load config:", err.Error())
+	}
+
+	dbDriver, dbSource := config.DBDriver, config.DBSource
+	database.ConnectDatabase(dbDriver, dbSource)
+
+	fiberApp.Get("/", func(c *fiber.Ctx) error {
+		panic("This panic is caught by fiber")
+	})
+
+	port := config.Port
+	log.Printf("Server starting at port %s.", port)
+	fiberApp.Listen(":" + port)
 }
