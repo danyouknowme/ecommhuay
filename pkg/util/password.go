7737c11fd3b2c6b7ee508744de533b9928d901ee
danyouknowme <thanathip.suw@gmail.com>
2022-08-12 00:02:31 +0700

    Added function compare password

--- pkg/util/password.go
+++ pkg/util/password.go
@@ -14,3 +14,7 @@ func HashPassword(password string) (string, error) {
 
 	return string(hashedPassword), nil
 }
+
+func CheckPassword(password string, hashedPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
