1919aa2e17ed8529d93ef905f8ffffcbf77fcadc
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-08-13 16:38:27 +0700

    Added get user api

--- pkg/handlers/user.go
+++ pkg/handlers/user.go
@@ -7,9 +7,15 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type UserResponse struct {
+	Username string `json:"username"`
+	FullName string `json:"full_name"`
+	Email    string `json:"email"`
+}
+
 func RegisterAPI(ctx *fiber.Ctx) error {
-	var newUser dbmodels.User
-	if err := ctx.BodyParser(&newUser); err != nil {
+	var req dbmodels.User
+	if err := ctx.BodyParser(&req); err != nil {
 		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"message": err.Error(),
 		})
@@ -17,15 +23,22 @@ func RegisterAPI(ctx *fiber.Ctx) error {
 
 	log.Printf("post: /api/v1/users/register")
 
-	err := dbmodels.Register(newUser)
+	err := dbmodels.Register(req)
 	if err != nil {
 		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"message": err.Error(),
 		})
 	}
 
+	userResponse := UserResponse{
+		Username: req.Username,
+		FullName: req.FullName,
+		Email:    req.Email,
+	}
+
 	return ctx.JSON(fiber.Map{
 		"message": "Create a new user successfully!",
+		"user":    userResponse,
 	})
 }
 
@@ -46,4 +59,25 @@ func LoginAPI(ctx *fiber.Ctx) error {
 	}
 
 	return ctx.Status(fiber.StatusOK).JSON(user)
+}
+
+func GetUserAPI(ctx *fiber.Ctx) error {
+	username := ctx.Params("username")
+
+	log.Printf("get: /api/auth/v1/users/%s", username)
+
+	user, err := dbmodels.GetUser(username)
+	if err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	userResponse := UserResponse{
+		Username: user.Username,
+		FullName: user.FullName,
+		Email:    user.Email,
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(userResponse)
 }
--- pkg/token/jwt_maker.go
+++ pkg/token/jwt_maker.go
@@ -3,6 +3,8 @@ package token
 import (
 	"github.com/danyouknowme/ecommerce/pkg/util"
 	"github.com/dgrijalva/jwt-go"
+	"github.com/gofiber/fiber/v2"
+	jwtware "github.com/gofiber/jwt/v3"
 )
 
 func CreateToken(username string) (string, error) {
@@ -18,4 +20,30 @@ func CreateToken(username string) (string, error) {
 
 	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
 	return jwtToken.SignedString([]byte(config.TokenSymmetricKey))
+}
+
+func AuthRequired() fiber.Handler {
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		return nil
+	}
+
+	return jwtware.New(jwtware.Config{
+		ErrorHandler:   AuthError,
+		SigningKey:     []byte(config.TokenSymmetricKey),
+		SuccessHandler: AuthSuccess,
+	})
+}
+
+func AuthSuccess(ctx *fiber.Ctx) error {
+	return ctx.Next()
+}
+
+func AuthError(ctx *fiber.Ctx, err error) error {
+	if err.Error() == "Missing or malformed JWT" {
+		return ctx.Status(fiber.StatusBadRequest).
+			JSON(fiber.Map{"status": "error", "message": "Missing or malformed JWT", "data": nil})
+	}
+	return ctx.Status(fiber.StatusUnauthorized).
+		JSON(fiber.Map{"status": "error", "message": "Invalid or expired JWT", "data": nil})
 }
